c80543d7e392cbce58d2c9411f2207157374dfcf
Thulio Fonseca <thulio50@hotmail.com>
2023-05-11 00:27:04 -0300

    Refatoracao e limpeza de codigo dos dispositivos IoT

--- ESP_LORA_DEVICES/EspLoRaReceiver/EspLoRaReceiver.ino
+++ ESP_LORA_DEVICES/EspLoRaReceiver/EspLoRaReceiver.ino
@@ -1,80 +1,112 @@
+// ----------------------------------- [ EspLoRa Receiver ] ---------------------------------------- //
+//
+//   Autor: Thulio Fonseca
+//   Curso: Sistemas de Informação - IFMG - Ouro Branco
+//   Descrição: Dispositivo IoT para recepção e retransmição de dados para o Nó Blockchain 
+//      
 
-/****** RX *********/
+// --------------------------------- [ Declaração de Bibliotecas ] ------------------------------------ //
 
 #include "Arduino.h"
 #include "heltec.h"
 
-#define LedStatus 02
+// --------------------------------- [ Definição de nomes para pinos de I/O ] ------------------------- //
+
+#define LedStatus 35
 #define TxPin 33
 #define RxPin 34
 
+// --------------------------------- [ Declaração de variáveis globais ] ------------------------------- //
+
 bool comunicacaoEstabelecida = false;
-bool SerialLogStatus = 0;
+bool SerialLogStatus = 1;
+
+// ------------------------------------------ [ Setup ] ------------------------------------------------ //
 
 void setup() {
 
+// Inicialização do Display OLED 0,96":
+
   Heltec.begin(true /*DisplayEnable Enable*/, false /*LoRa Disable*/, false /*Serial Enable*/);
-  Heltec.display->setFont(ArialMT_Plain_10);
-  
+  Heltec.display->setFont(ArialMT_Plain_10);  
+
+// Definição de funções dos pinos I/O:
 
   pinMode(RxPin, INPUT);
   pinMode(TxPin, OUTPUT);
   pinMode(LedStatus, OUTPUT);
 
+ // Inicialização da comunicação Serial:
+
   Serial.begin(9600);
-  SerialLog("Comunicacao serial iniciada!", SerialLogStatus);
-
+  Serial.flush();
+  
   Serial2.begin(9600, SERIAL_8N1, 34, 33);
-  SerialLog("Módulo LoRa TX iniciado", SerialLogStatus);
+  Serial2.flush();
 
-  drawProgressBarStartup();
+  ExibirBarraProgresso();
 
-  comunicacaoEstabelecida = messageScan();
+  comunicacaoEstabelecida = BuscarConexao();
 
 }
 
+// ------------------------------------------ [ Loop ] ------------------------------------------------ //
+
 void loop() {
 
-  while(comunicacaoEstabelecida){
+  while(comunicacaoEstabelecida){  
 
-      int aguardaResposta = 0;
-            
-      while(!Serial2.available() >= 1 && aguardaResposta < 30) {
-      
-        SerialLog("Aguardando Conexão - " + (String)aguardaResposta, SerialLogStatus);
-        delay(250);
-        aguardaResposta++;
-        
+    int aguardaResposta = 0;
+
+    // Aguardando contato do transmissor:
+          
+    while(!Serial2.available() >= 1 && aguardaResposta < 30) {        
+      analogWrite(LedStatus, 0); 
+      delay(250);
+      aguardaResposta++;        
     }
+
+     // Verifica se o tempo limite de espera de conexão foi atingido:
 
     if(aguardaResposta >= 27){
       comunicacaoEstabelecida = false;
       continue;
     }
 
-    String message = Serial2.readString();
-    Serial2.flush();
-
-    if(message == "ping")
-      comunicacaoEstabelecida = false;    
+    // Lê a menssagem do Transmissor:
     
-    SerialLog("Recebendo dados: " + message, SerialLogStatus);
+    analogWrite(LedStatus, 48); 
+    String message = Serial2.readString();
+
+    // Verifica o conteúdo:
+    
+    if(message == "ping"){      
+      comunicacaoEstabelecida = false;
+      continue;
+    }          
+
+    // Envia do Payload recebido para Nó Blockchain via Serial:
+    
+    SerialLog(message, SerialLogStatus);
     ShowLogDisplay(message);    
 
+    // Envia do confirmação via LoRa:
+    
     Serial2.write("ACK");
     Serial2.flush();        
   }
-
-  comunicacaoEstabelecida = messageScan();
+  
+  //Tenta reconetar:
+  comunicacaoEstabelecida = BuscarConexao();
 }
 
-bool messageScan(){
+bool BuscarConexao(){
   
-  bool sucesso = false;
+  bool sucessoConexao = false;
 
-  while(!sucesso){  
+  while(!sucessoConexao){  
 
-    ShowLogDisplay("Aguardando ping...");
+    ShowLogDisplay("Aguardando contato...");
 
     if(Serial2.available() >= 1 ){
       
@@ -83,19 +115,17 @@ bool messageScan(){
   
       if(message == "ping"){
         
-        digitalWrite(LedStatus, LOW);      
+        analogWrite(LedStatus, 48);      
         Serial2.print("pong");
         Serial2.flush();
         
-        sucesso = true;
-        SerialLog("Comunicação estabelecida-> " + message + " recebido", SerialLogStatus); 
-        ShowLogDisplay("Ping Recebido!!!");          
+        sucessoConexao = true;
+        ShowLogDisplay("Contato Recebido!!!");          
       }
       else {
         
-        digitalWrite(LedStatus, HIGH);
-        SerialLog("Aguardando mensagem ping -> " + message, SerialLogStatus);
-        ShowLogDisplay("Mensagem Invalida");  
+        analogWrite(LedStatus, 0);
+        ShowLogDisplay("Mensagem Invalida"); 
       
       }     
     }
@@ -104,11 +134,11 @@ bool messageScan(){
     
   };
   
-  return sucesso; 
+  return sucessoConexao; 
   
 }
 
-void drawProgressBarStartup() {
+void ExibirBarraProgresso() {
 
   int counter = 1;
   while(counter < 500){
@@ -120,7 +150,7 @@ void drawProgressBarStartup() {
 
   // draw the percentage as String
   Heltec.display->setTextAlignment(TEXT_ALIGN_CENTER);
-  Heltec.display->drawString(64, 15, String(progress) + "%");
+  Heltec.display->drawString(64, 15, "Iniciando Receptor - " + String(progress) + "%");
   Heltec.display->display();
   
   counter++;
@@ -135,7 +165,7 @@ void ShowLogDisplay(String messageToShow){
     Heltec.display->clear();  
     Heltec.display->setTextAlignment(TEXT_ALIGN_LEFT);
     Heltec.display->setFont(ArialMT_Plain_10);
-     Heltec.display->drawStringMaxWidth(0, 0, 128, messageToShow);
+    Heltec.display->drawStringMaxWidth(0, 0, 128, messageToShow);
     Heltec.display->display();
     
 }
